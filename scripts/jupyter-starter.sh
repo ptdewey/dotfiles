a649687b39b6cd40a786ed12aeefa1725bc0b27a
Patrick <57921252+TheSupremeToast@users.noreply.github.com>
2022-08-06 13:33:55 -0400

    refactor(DOT-0): reliability tweaks and fixes (#1)
    * refactor(DOT-0): added file exists checks in shell rc files
    
    * fix(DOT-0): fixed directory issue with jupyter script
    
    * fix(DOT-0): fixed directory issues in setup.sh

--- scripts/jupyter-starter.sh
+++ scripts/jupyter-starter.sh
@@ -2,14 +2,15 @@
 
 cinit
 
-echo "Enter (1) for ~/Documents/python/ "
+echo "Enter (1) for ~/Documents/projects/ "
 echo "or (2) for ~/Documents/school/CMDA3605/"
 echo "or (3) for ~/Documents/school/CMDA3634/"
 echo "or (4) for ~/Documents/school/CMDA3654/"
+echo "or (5) for ~/Documents/python"
 read usr_input
 
 if [ $usr_input -eq 1 ] ; then
-    cd ~/Documents/python
+    cd ~/Documents/projects
 
 elif [ $usr_input -eq 2 ] ; then
     cd ~/Documents/school/CMDA3605
@@ -17,8 +18,10 @@ elif [ $usr_input -eq 2 ] ; then
 elif [ $usr_input -eq 3 ] ; then
     cd ~/Documents/school/CMDA3634
 
-elif [ $usr_input -eq 2 ] ; then
-    cd ~/Documents/school/CMDA3654
+elif [ $usr_input -eq 4 ] ; then
+    cd ~/Documents/school/cmda3654
+elif [ $user_input -eq 5]; then
+    cd ~/Documents/python
 fi
 
 # conda activate
