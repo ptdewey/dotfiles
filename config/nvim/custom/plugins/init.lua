93e3b05e01366ea309cc529dc9cca747b68b60be
TheSupremeToast <patrickdewey2002@gmail.com>
2023-01-20 10:52:28 -0500

    updates to nvim config

--- config/nvim/custom/plugins/init.lua
+++ config/nvim/custom/plugins/init.lua
@@ -1,10 +1,18 @@
 return  {
-  -- discord presence
-  ["andweeb/presence.nvim"] = { auto_update = true },
-  ["folke/todo-comments.nvim"] = {
-    config = function()
-      require("todo-comments").setup()
-    end
-  },
+    -- discord presence
+    ["andweeb/presence.nvim"] = { auto_update = true },
+    ["folke/todo-comments.nvim"] = {
+        config = function()
+            require("todo-comments").setup()
+        end
+    },
+    -- require("nvim-web-devicons").set_icon {
+    --     cu = {
+    --         icon = "cuda", -- nvidia logo
+    --         color= "#76B900", -- green
+    --         cterm_color = "65", -- ???
+    --         name = "Cuda"
+    --     }
+    -- }
 }
 
