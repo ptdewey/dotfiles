70a875ee69cc7a7becb894b9edcbeffda98ec77d
Patrick Dewey <patrickdewey@vt.edu>
2024-02-11 17:03:54 -0500

    nvim tweaks and template fix

--- config/nvim/lua/plugin/knitr.lua
+++ config/nvim/lua/plugin/knitr.lua
@@ -3,7 +3,7 @@ return {
     "ptdewey/knitr-nvim",
 
     -- branch = "dev",
-    -- branch = "main",
+    branch = "main",
 
     -- load for correct file types only
     ft = { "rmd", "r" },
@@ -13,7 +13,7 @@ return {
     },
 
     config = function()
-        require("knitr")
+        require("knitr").setup()
         -- Map to F6/F7
         vim.keymap.set("n", "<F6>", ":KnitRpdf<CR>", { noremap = true })
         vim.keymap.set("n", "<F7>", ":KnitRhtml<CR>", { noremap = true })
