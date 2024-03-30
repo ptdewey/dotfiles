e845190301c403ee3a31508663dbe9a892c4dd58
ptdewey <patrickdewey2002@gmail.com>
2024-03-30 18:02:23 -0400

    treesitter modifications

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -9,24 +9,34 @@
     home.username = "patrick";
     home.homeDirectory = "/home/patrick";
 
+    gtk.enable = true;
+    # gtk.cursorTheme.package = pkgs.bibata-cursors;
+    # gtk.cursorTheme.name = "Bibata-Modern-Ice";
+    #
+    gtk.theme.package = pkgs.adw-gtk3;
+    gtk.theme.name = "adw-gtk3";
+
+    # gtk.iconTheme.package = gruvboxPlus;
+    # gtk.iconTheme.name = "GruvboxPlus";
+
     # Install nix packages into environment
     home.packages = with pkgs; [
-        bspwm
+        # bspwm
         csvlens
-        dunst
+        # dunst
         fd
-        feh
+        # feh
         fzf
         glow
         lsd
-        neovim
-        polybar
-        picom
-        pywal
+        # neovim
+        # polybar
+        # picom
+        # pywal
         ripgrep
         rofi
         rustup
-        sxhkd
+        # sxhkd
         tokei
     ];
 
