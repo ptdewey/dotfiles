65360e759811f99fdfe675d50327d7265fd197c3
ptdewey <patrickdewey2002@gmail.com>
2024-04-02 14:15:32 -0400

    feat(cmp menu follows cursor)

--- config/nvim/lua/plugin/cmp.lua
+++ config/nvim/lua/plugin/cmp.lua
@@ -26,6 +26,11 @@ return {
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
         cmp.setup {
+            view = {
+                entries = {
+                    follow_cursor = true,
+                },
+            },
             luasnip.config.setup {},
             snippet = {
                 expand = function(args)
