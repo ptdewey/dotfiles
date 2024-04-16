2e10bab5b8354adbcd4f2d64cc16eac07324a7cb
ptdewey <patrickdewey2002@gmail.com>
2024-04-16 09:08:48 -0400

    removed theming from home manager

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -10,14 +10,7 @@
     home.homeDirectory = "/home/patrick";
 
     gtk.enable = true;
-    # gtk.cursorTheme.package = pkgs.bibata-cursors;
-    # gtk.cursorTheme.name = "Bibata-Modern-Ice";
-    #
-    gtk.theme.package = pkgs.adw-gtk3;
-    gtk.theme.name = "adw-gtk3";
 
-    # gtk.iconTheme.package = gruvboxPlus;
-    # gtk.iconTheme.name = "GruvboxPlus";
 
     # Install nix packages into environment
     home.packages = with pkgs; [
