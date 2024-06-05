78538bd0aa76399ed419c7b9841be72e2b5b73c9
ptdewey <patrickdewey2002@gmail.com>
2024-06-05 09:53:37 -0400

    feat: added new cmp sources and added source to dropdown

--- config/nvim/lua/plugin/cmp.lua
+++ config/nvim/lua/plugin/cmp.lua
@@ -1,28 +1,34 @@
 -- Autocompletion
 return {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
         -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
 
         -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
+        "hrsh7th/cmp-nvim-lsp",
 
         -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
+        "rafamadriz/friendly-snippets",
 
-        'hrsh7th/cmp-buffer',
+        -- Buffer source
+        "hrsh7th/cmp-buffer",
 
+        -- Path source
+        "hrsh7th/cmp-path",
+
+        -- nvim lua api
+        "hrsh7th/cmp-nvim-lua",
 
         -- completion source from dictionary
-        'octaltree/cmp-look',
+        "octaltree/cmp-look",
     },
 
     -- configure plugin
     config = function ()
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
         local has_words_before = function()
             unpack = unpack or table.unpack
@@ -32,6 +38,8 @@ return {
         cmp.setup {
             view = {
                 entries = {
+                    name = 'custom',
+                    selection_order = 'near_cursor',
                     follow_cursor = true,
                 },
             },
@@ -43,18 +51,18 @@ return {
             },
             preselect = cmp.PreselectMode.None,
             mapping = cmp.mapping.preset.insert {
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-y>'] = cmp.mapping.complete {},
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<C-h>'] = cmp.mapping.confirm({ select = true }),
-                -- ['<S-CR>'] = cmp.mapping.confirm {
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-y>"] = cmp.mapping.complete {},
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-h>"] = cmp.mapping.confirm({ select = true }),
+                -- ["<S-CR>"] = cmp.mapping.confirm {
                 --     behavior = cmp.ConfirmBehavior.Replace,
                 --     select = true,
                 -- },
-                ['<Tab>'] = cmp.mapping(function(fallback)
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                         -- elseif luasnip.expand_or_locally_jumpable() then
@@ -64,8 +72,8 @@ return {
                     else
                         fallback()
                     end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                         -- elseif luasnip.locally_jumpable(-1) then
@@ -73,13 +81,14 @@ return {
                     else
                         fallback()
                     end
-                end, { 'i', 's' }),
+                    end, { "i", "s" }),
             },
+
             sources = {
                 { name = "luasnip" },
-                { name = "nvim_lsp" },
-                { name = "treesitter" },
                 { name = "nvim_lua" },
+                { name = "nvim_lsp" },
+                -- { name = "treesitter" },
                 { name = "path" },
                 { name = "buffer" },
                 {
@@ -88,19 +97,24 @@ return {
                     option = {
                         convert_case = true,
                         loud = true,
-                        --dict = '/usr/share/dict/words',
+                        --dict = "/usr/share/dict/words",
                     },
                 },
             },
+
+            formatting = {
+                format = function(entry, vim_item)
+                    vim_item.menu = ({
+                        buffer = "[buf]",
+                        nvim_lsp = "[lsp]",
+                        luasnip = "[snip]",
+                        nvim_lua = "[api]",
+                        path = "[path]",
+                        look = "[dict]",
+                    })[entry.source.name]
+                    return vim_item
+                end
+            },
         }
-
-        -- NOTE: commented out to fix that annoying autocomplete issue
-        -- cmp.setup.cmdline({ '/', '?' }, {
-        --     mapping = cmp.mapping.preset.cmdline(),
-        --     sources = {
-        --         { name = 'buffer' }
-        --     }
-        -- })
     end
-
 }
