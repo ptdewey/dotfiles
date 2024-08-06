8fca071e81aebad63a925d9e347211ec545fed16
ptdewey <patrickdewey2002@gmail.com>
2024-08-06 08:19:57 -0400

    refactor: switched puml ftdetect to lua

---
+++ config/nvim/ftdetect/plantuml.lua
@@ -0,0 +1,18 @@
+vim.filetype.add({
+    extension = {
+        ["puml"] = "plantuml",
+        ["pu"] = "plantuml",
+        ["plantuml"] = "plantuml",
+        ["wsd"] = "plantuml",
+        ["iuml"] = "plantuml",
+    },
+    filename = {},
+    pattern = {},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "plantuml",
+    callback = function()
+        vim.opt_local.commentstring = "' %s"
+    end
+})
