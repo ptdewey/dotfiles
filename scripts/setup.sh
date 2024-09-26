deaa2c0575beb9b86f30c6b35ed113205b27ecd3
ptdewey <patrickdewey2002@gmail.com>
2024-09-26 16:44:59 -0400

    refactor: rewrote setup script for better organization

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,29 +8,10 @@
 dir="$HOME/dotfiles"
 # old dotfiles backup directory
 olddir="$HOME/Documents/dotfiles_old"
+
 # list of files/folders to symlink in homedir
-
-# Ask if user is on server machine or not (if cli arg not provided)
-# (only copies necessary server config)
-if [ -n "$1" ]; then
-    confirmation="$1";
-else
-    echo "Is this a server machine? (y/n)"
-    read -r confirmation
-fi
-
-# TODO: make files and directory linking better, possibly with stow
-if [[ "$confirmation" == "y" || "$confirmation" == "Y" ]]; then
-    echo "Running in server configuration mode..."
-    files="bashrc vimrc zshrc ignore tmux.conf"
-    config_directories="nvim nixpkgs nix tmux"
-    directories="templates"
-else
-    echo "Running in laptop/desktop configuration mode..."
-    files="bashrc vimrc zshrc xinitrc Xresources ignore tmux.conf lintr Rprofile"
-    config_directories="bspwm rofi polybar kitty nvim zathura wezterm tmux nix nixpkgs"
-    directories="templates"
-fi
+files=$(ls "${DIR}/home")
+config_directories=$(ls "${DIR}/config")
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -47,7 +28,7 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv "$HOME/.$file" "$olddir/"
     echo "Creating symlink to $file in home directory."
-    ln -s "$dir/$file" "$HOME/.$file"
+    ln -s "$dir/home/$file" "$HOME/.$file"
 done
 echo "...done"
 
@@ -57,14 +38,6 @@ for directory in $config_directories; do
     mv "$HOME/.config/$directory" "$olddir/config"
     echo "Creating symlink to $dir in .config"
     ln -s "$dir/config/$directory" "$HOME/.config/"
-done
-
-# symlink files and directories to ~/
-for directory in $directories; do
-    echo "Moving any existing dotfiles from ~ to $olddir/"
-    mv "$HOME/$directory" "$olddir/"
-    echo "Creating symlink to $dir in ~/"
-    ln -s "$dir/$directory" "$HOME/$directory"
 done
 
 # special case for symlinking fonts
@@ -77,6 +50,8 @@ echo "Making common directories..."
 mkdir "$HOME/projects"
 mkdir "$HOME/work"
 mkdir "$HOME/school"
+
+# TODO: install go programs (and clone git directories)?
 
 echo "...done"
 
