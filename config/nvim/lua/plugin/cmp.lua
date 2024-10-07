7b0861bf0a3604d2b86025df298dbe928ea5181d
ptdewey <patrickdewey2002@gmail.com>
2024-10-07 15:02:44 -0400

    changed cmp keymaps

--- config/nvim/lua/plugin/cmp.lua
+++ config/nvim/lua/plugin/cmp.lua
@@ -62,7 +62,8 @@ return {
                     ["<C-y>"] = cmp.mapping.complete({}),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<C-h>"] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
+                    -- ["<Tab>"] = cmp.mapping(function(fallback)
+                    ["<C-j>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
                             -- elseif luasnip.expand_or_locally_jumpable() then
@@ -73,7 +74,8 @@ return {
                             fallback()
                         end
                     end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    ["<C-k>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
                             -- elseif luasnip.locally_jumpable(-1) then
--- config/nvim/lua/plugin/luasnip.lua
+++ config/nvim/lua/plugin/luasnip.lua
@@ -20,25 +20,25 @@ return {
 
             -- keymaps for navigating editable regions
             -- jump down
-            vim.keymap.set({ "i", "s" }, "<C-j>", function()
+            vim.keymap.set({ "i", "s" }, "<A-j>", function()
                 if ls.expand_or_jumpable() then
                     ls.expand_or_jump()
                 end
-            end, { silent = true })
+            end, { silent = true, noremap = true })
 
             -- jump up
-            vim.keymap.set({ "i", "s" }, "<C-k>", function()
+            vim.keymap.set({ "i", "s" }, "<A-k>", function()
                 if ls.jumpable(-1) then
                     ls.jump(-1)
                 end
-            end, { silent = true })
+            end, { silent = true, noremap = true })
 
             -- cycle choices
-            vim.keymap.set({ "i", "s" }, "<C-l>", function()
+            vim.keymap.set({ "i", "s" }, "<A-l>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
                 end
-            end, { silent = true })
+            end, { silent = true, noremap = true })
         end,
     },
 }
