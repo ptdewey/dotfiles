4cbf807c116d35f007fb9013a2d9ce3619098326
ptdewey <57921252+ptdewey@users.noreply.github.com>
2025-07-02 16:12:08 -0400

    nvim testing plugins

---
+++ config/nvim/lua/plugin/tests.lua
@@ -0,0 +1,96 @@
+return {
+    {
+        "andythigpen/nvim-coverage",
+        -- TODO: load on custom event where cover.out file exists (or on "Coverage" command)
+        command = { "Coverage", "CoverageLoad", "CoverageToggle", "CoverageShow", "CoverageSummary" },
+        config = function()
+            require("coverage").setup({
+                auto_reload = true,
+                commands = true,
+                lang = {
+                    go = {
+                        coverage_file = "cover.out"
+                    }
+                }
+            })
+        end,
+    },
+    {
+        "nvim-neotest/neotest",
+        command = { "Neotest" },
+        keys = {
+            { "<leader>tr", desc = "[T]est [R]un" },
+            { "<leader>ts", desc = "[T]est [S]etup" },
+            { "<leader>th", desc = "[T]est [H]ere" },
+            { "<leader>ta", desc = "[T]est [A]ll" },
+            { "<leader>tc", desc = "[T]oggle [C]overage" },
+        },
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            {
+                "fredrikaverpil/neotest-golang",
+                dependencies = {
+                    "andythigpen/nvim-coverage"
+                },
+            },
+        },
+
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-golang")({
+                        runner = "go",
+                        go_test_args = {
+                            "-v",
+                            "-race",
+                            "-count=1",
+                            "-coverpkg=./...",
+                            "-coverprofile=" .. vim.fn.getcwd() .. "/cover.out",
+                        },
+                    }),
+                },
+            })
+
+            local coverage_loaded = false
+            local setup_tests = function()
+                local win = vim.api.nvim_get_current_win()
+                if not coverage_loaded then
+                    require("coverage").load(false)
+                    coverage_loaded = true
+                end
+                require("coverage").toggle()
+                require("neotest").summary.toggle()
+                vim.api.nvim_set_current_win(win)
+            end
+
+            vim.api.nvim_create_user_command("TestsSetup", function() setup_tests() end,
+                { desc = "Open the testing summary and start show coverage" })
+            vim.api.nvim_create_user_command("TestsRunFile",
+                function() require("neotest").run.run(vim.fn.expand("%")) end,
+                { desc = "Run all tests in the current file with neotest" })
+
+            vim.keymap.set("n", "<leader>th", function()
+                    require("neotest").run.run()
+                end,
+                { desc = "[T]est [H]ere" })
+            -- TODO: make this run alternate file tests as well (i.e. in foo.go run foo_test.go, otherwise do nothing)
+            vim.keymap.set("n", "<leader>tr", function()
+                    require("neotest").run.run(vim.fn.expand("%"))
+                    -- run_file_tests()
+                end,
+                { desc = "[T]est [R]un", silent = true })
+            vim.keymap.set("n", "<leader>ta", function() require("neotest").run.run({ suite = true }) end,
+                { desc = "[T]est [A]ll" })
+            vim.keymap.set("n", "<leader>ts", setup_tests,
+                { desc = "[T]ests [S]etup", silent = true })
+            vim.keymap.set("n", "<leader>tc", function() require("coverage").toggle() end,
+                { desc = "[T]oggle [C]overage" })
+
+            -- TODO: "<leader>tf" for jumping to paired test file (like that one plugin does, shouldn't be too hard for go)
+        end,
+    },
+}
--- home/hammerspoon/init.lua
+++ home/hammerspoon/init.lua
@@ -26,22 +26,28 @@ end)
 --
 -- Navigation
 --
-local function focusApp(appName)
+local function focusApp(appName, opts)
     local app = hs.application.get(appName)
     if app then
         app:activate()
     else
-        hs.alert.show("App not running: " .. appName)
+        if opts and opts.open then
+            hs.application.open(appName)
+        else
+            hs.alert.show("App not running: " .. appName)
+        end
         -- TODO: open application if not running (optionally with param?)
     end
 end
 
 -- Focus hotkeys
-hs.hotkey.bind({ "ctrl" }, "T", function() focusApp("WezTerm") end)
-hs.hotkey.bind({ "ctrl" }, "B", function() focusApp("Zen") end) -- NOTE: switch this key to open zen
-hs.hotkey.bind({ "ctrl" }, "X", function() focusApp("Slack") end)
-hs.hotkey.bind({ "ctrl", "cmd" }, "e", function() focusApp("Outlook") end)
-hs.hotkey.bind({ "ctrl", "cmd" }, "d", function() focusApp("Finder") end)
+hs.hotkey.bind({ "ctrl" }, "t", function() focusApp("WezTerm", { open = true }) end)
+hs.hotkey.bind({ "ctrl" }, "b", function() focusApp("Zen", { open = true }) end)
+hs.hotkey.bind({ "ctrl" }, "s", function() focusApp("Slack", { open = true }) end)
+hs.hotkey.bind({ "ctrl", "shift" }, "x", function() focusApp("Slack", { open = true }) end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "x", function() focusApp("Slack", { open = true }) end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "e", function() focusApp("Outlook", { open = true }) end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "d", function() focusApp("Finder", { open = true }) end)
 
 -- "Workspace" navigation
 -- TODO:
